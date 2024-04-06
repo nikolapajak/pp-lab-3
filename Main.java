abb788d6c5d09acfa421b18b80ec36be3140511b
Nikola <nikolapajak2706@gmail.com>
2024-04-06 18:53:54 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Point point = new Point(3.5, 2.0);
+
+        System.out.println("Współrzędna x: " + point.getX());
+        System.out.println("Współrzędna y: " + point.getY());
+    }
+}
